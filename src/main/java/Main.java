da06e1bd3eb55757fd1e745fe0b412c8866c3d1f
Anna Strelkova <115898741+anyute@users.noreply.github.com>
2023-07-20 02:37:13 +0300

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,24 @@
-import java.io.File;
-import java.util.Arrays;
-
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
-
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8989)) {
+            BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream())
+                ) {
+                    String request = in.readLine();
+                    String answer = engine.search(request).toString();
+                    out.println(answer);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -1,7 +1,45 @@
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class PageEntry implements Comparable<PageEntry> {
     private final String pdfName;
     private final int page;
     private final int count;
 
-    // ???
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+        return Integer.compare(o.getCount(), this.getCount());
+    }
+
+    @Override
+    public String toString() {
+        Map map = new LinkedHashMap();
+        map.put("pdfName", pdfName);
+        map.put("page", page);
+        map.put("count", count);
+        JSONObject result = null;
+        try {
+            result = new JSONObject(map);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        // Для работы с json мы используем библиотеку jettison.
+        // Эта библиотека использует LinkedHashMap, который поддерживает порядок атрибутов.
+        // Поэтому метод toString выводит атрибуты в том порядке, в котором они добавлялись изначально.
+        return result.toString();
+    }
 }
